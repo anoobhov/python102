d56a023e7f15af4c8aff93871d4845b58ff62063
Anubhav Raj <anubhavr219@gmail.com>
2024-07-03 14:38:31 +0000

    for and while

---
+++ main3.py
@@ -0,0 +1,57 @@
+# WAP to print number from 1 to 10
+# for i in range(1,11):
+#     print(i)
+
+# WAP to print even numbers
+# for i in range(2,11,2):
+#     print(i)
+
+# WAP to print in reverse way
+# for i in range(10,0,-1):
+#     print(i)
+
+# WAP to print multiplication of 3
+# print("MULTIPLICATION TABLE FOR 3")
+# for i in range(1, 11):
+#     print(3, " x", i, " =", 3*i)
+
+# WAP to print multiplication of 1 to 5
+# for i in range(1, 6):
+#     print("Multiplication table of ",i)
+#     for j in range(1, 11):
+#         print(i, " x", j," =", i*j)
+
+# Pattern printing
+"""
+print this
+*
+* *
+* * *
+* * * *
+"""
+# for i in range(1,5):
+#     for j in range(1,i+1):
+#         print("* ",end=" ")
+#     print("")
+
+"""
+now print this
+      *     
+    * *
+  * * *
+* * * *
+"""
+# for i in range (0, 5):
+#     for j in range(0,5):
+#         if j >=3-i:
+#             print(" *",end="")
+#         else:
+#             print("  ",end="")
+#     print("")
+
+i=0
+while i<10:
+    print(i)
+    i += 1
+
+
